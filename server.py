a197605a5ae89e263f581a55f7681d9267363bd6
Nutto.jsx <91049342+ntdotjsx@users.noreply.github.com>
2024-06-20 23:03:14 +0700

    Update server.py

--- server.py
+++ server.py
@@ -1,22 +1,27 @@
+# server.py
 import socket
 
-HOST = '0.0.0.0'
+# กำหนด IP address และ port ที่จะใช้สร้างเซิร์ฟเวอร์
+HOST = '0.0.0.0'  # ใช้ '0.0.0.0' เพื่อให้รับ connection จากทุก interface ของเครื่อง
 PORT = 12345
 
+# สร้าง socket object
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-
+    # Bind เซิร์ฟเวอร์ไปยัง address และ port ที่กำหนด
     s.bind((HOST, PORT))
+    # Listen รอการเชื่อมต่อเข้ามา
     s.listen()
     print(f'Server is listening on {HOST}:{PORT}')
-
-    #Accept Connect
+    # Accept การเชื่อมต่อ
     conn, addr = s.accept()
     with conn:
-        print('Connect by', addr)
+        print('Connected by', addr)
         while True:
+            # รับข้อมูลจาก client
             data = conn.recv(1024)
             if not data:
                 break
+            # แสดงข้อมูลที่รับมา
             print(f"Received from client: {data.decode()}")
-
+            # ส่งข้อมูลกลับไปยัง client
             conn.sendall(data)
